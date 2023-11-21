3dd27b406c4345b903e6dffc86f47f9a50a77b5c
sanjueranga <sanjueranga16@gmail.com>
2023-11-21 17:18:24 +0530

    front api

--- frontend/config/api.ts
+++ frontend/config/api.ts
@@ -11,8 +11,21 @@ export async function getUser() {
     throw new Error("Failed to submit user data");
   }
   console.log("success");
-  console.log(res + " here ++++++++++++++++++++++++++++=");
   return res;
 }
 
+export async function postScore(data: any, id: number) {
+  const res = await fetch(`${server}/users/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) {
+    throw new Error("Failed to submit user data");
+  }
+  return await res.json();
+}
 
+// meka use karaddi, postScore(user.id, data) vidiyt call krhn, data kiyn eka, {"emotional":10}, kiyn fomat eken one
