e8d7dc66aa68e2606c4b653e97557164e22bf07a
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-01-01 18:32:05 +0530

    added previous button

--- frontend/app/(public)/(taketest)/social/page.tsx
+++ frontend/app/(public)/(taketest)/social/page.tsx
@@ -151,6 +151,20 @@ const Social = () => {
                     />
                   ))}
                 </div>
+
+                <Link href="/spiritual">
+                  <button
+                    className={`py-3 px-10 mr-4 mt-10 bg-emerald-500 inline-block rounded-full ${
+                      isLoading
+                        ? "opacity-50 cursor-not-allowed"
+                        : "hover:bg-emerald-600"
+                    } text-white`}
+                    disabled={isLoading}
+                  >
+                    {isLoading ? "Loading..." : "Previous Page"}
+                  </button>
+                </Link>
+
                 <Link href="/emotional" onClick={handleNext}>
                   <button
                     className={`py-3 px-10 mt-10 bg-emerald-500 inline-block rounded-full ${
