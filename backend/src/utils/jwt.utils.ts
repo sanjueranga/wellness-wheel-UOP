2fee208c0c28a99a8afc3dc3fc1fb0cbc12258ab
sanjueranga <sanjueranga16@gmail.com>
2024-01-05 17:59:06 +0530

    WIP

--- backend/src/utils/jwt.utils.ts
+++ backend/src/utils/jwt.utils.ts
@@ -1,14 +1,12 @@
-// jwt.util.ts
-
 import * as jwt from 'jsonwebtoken';
 
-const secretKey = process.env.SECRET;
-
 export const generateJwtToken = (payload: Record<string, any>): string => {
-  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+  const secretKey = process.env.SECRET;
+  return jwt.sign(payload, secretKey, { expiresIn: '7d' });
 };
 
 export const getUserIdFromToken = (token: string): number => {
+  const secretKey = process.env.SECRET;
   const decoded: any = jwt.verify(token, secretKey);
   return decoded.sub;
 };
