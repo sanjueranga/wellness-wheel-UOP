1b610b5041c338aacae2df78876349167d6f527b
sanjueranga <sanjueranga16@gmail.com>
2024-01-07 11:39:40 +0530

    fix: node packages fix

--- backend/src/utils/jwt.strategy.ts
+++ backend/src/utils/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'rahasak',
+      secretOrKey: process.env.SECRET,
     });
   }
 
